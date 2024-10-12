deec1c6f4d4e0b5230c65de11e748980c7632c43
kanav9063 <kanav@ucsb.edu>
2024-10-12 00:41:16 -0700

    first commit

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-red-500 text-white p-4">Hello from new project</div>
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
+}
